8324c260d87d255613ccb9bf49bbaa1e98ada25f
ronishadulal <ronishadulal10@gmail.com>
2023-03-01 16:28:25 +0545

    update

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 c = input("enter the desire letter ")
-if c in vowels:
+if c.lower in vowels:
     print( c, "is a vowel")
 else:
     print( c, "is a consonant")    
