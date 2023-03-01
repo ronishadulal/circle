0f9124a614280f6a4c08305fe0218e2a553ae829
ronishadulal <ronishadulal10@gmail.com>
2023-03-01 16:22:07 +0545

    update

--- task1.py
+++ task1.py
@@ -2,5 +2,5 @@ x1 = int(input(" enter value for x1 "))
 x2 = int(input(" enter value for x2 "))
 y1 = int(input(" enter value for y1 "))
 y2 = int(input(" enter value for y2 "))
-distance = ((x2 - x1)**2 - (y2 - y1)**2)**1/2
+distance = ((x2 - x1)**2 + (y2 - y1)**2)**1/2
 print("distance between the points is", distance)
