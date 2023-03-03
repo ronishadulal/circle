065c2f28d3d2b6c54823fead81f7dab37130c805
ronishadulal <ronishadulal10@gmail.com>
2023-03-03 12:54:33 +0545

    update

--- assignment3.py
+++ assignment3.py
@@ -1,14 +1,11 @@
 #sort given list without using python method
 a = [54, 1, 12, 99, 20, 13, 5, 100]
-aa = []
-while a:
-    minimum = a[0]
-    for x in a:
-        if x < minimum:
-            minimum = x
-    aa.append(minimum)
-    a.remove(minimum)
-print(aa) 
+
+for x in range(0,len(a)):
+    for y in range (x +1, len(a)):
+        if (a[x]>a[y]):
+            a[x],a[y]=a[y],a[x]
+print(a)         
 
 
 #create a number whose digits are the elements of list
